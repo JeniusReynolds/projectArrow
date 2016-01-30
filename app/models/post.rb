76b0884de96bad93a9ef826f938c35c4fe0b96d1
Jonathan Reynolds <J.Reynolds407@gmail.com>
2016-01-30 17:01:08 -0500

    Added devise logic to program

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ActiveRecord::Base
+	validates :user_id, presence: true 
 	validates :image, presence: true
 	
 	has_attached_file :image, styles: { medium: "640x >", thumb: "100x100>" }
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  	belongs_to :user 
+
 end
