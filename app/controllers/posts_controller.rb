6e7b54e4350dfb21d4996f977ba5c1b14827a73f
Jonathan Reynolds <J.Reynolds407@gmail.com>
2016-01-30 23:55:57 -0500

    Added logic to protect user content from being deleted by other users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :owned_post, only: [:edit, :update, :destroy]  
 
   # GET /posts
   # GET /posts.json
@@ -72,4 +73,12 @@ class PostsController < ApplicationController
     def post_params
       params.require(:post).permit(:caption, :image)
     end
+
+    def owned_post  
+      unless current_user == @post.user
+        flash[:alert] = "That post doesn't belong to you!"
+        redirect_to root_path
+      end
+    end
+
 end
